4677c3969c48d8b204626ad80fd190dec21fdf23
Erik Newman <ejnewman@gmail.com>
2011-03-12 12:54:13 -0800

    Initial file reading

--- src/UtexasParser.java
+++ src/UtexasParser.java
@@ -1,3 +1,9 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.Scanner;
+
 
 public class UtexasParser {
 
@@ -5,9 +11,23 @@ public class UtexasParser {
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		System.out.println("WTF?");
-
+		try{
+			String infilename = "C:\\Users\\Erik\\Documents\\Long Term Static\\calero_longterm.OUT";
+			File inFile = new File(infilename);
+			Scanner in = new Scanner(inFile);
+			//BufferedReader in = new BufferedReader(new FileReader(infilename));
+			String str;
+			while ((str = in.nextLine()) != null){
+				//System.out.println(str);
+				if(str.contains("Profile Line No.")){
+					// found profile lines
+					String[] tokens = str.split("[ ]+");
+					System.out.println(tokens[4]);
+					
+				}
+			}
+			 in.close();
+		} catch(IOException e){
+		}
 	}
-
-}
+		}
