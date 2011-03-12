214456ad2c45f86d2d2570e00ef014fc7a0b04d7
Erik Newman <ejnewman@gmail.com>
2011-03-12 13:55:41 -0800

    Geometry classes and parsing

---
+++ src/Line.java
@@ -0,0 +1,10 @@
+import java.util.List;
+
+
+public class Line {
+	List<Point> points;
+	
+	public Line(List<Point> points){
+		this.points = points;
+	}
+}
--- src/UtexasParser.java
+++ src/UtexasParser.java
@@ -2,6 +2,8 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import java.util.Scanner;
 
 
@@ -19,15 +21,41 @@ public class UtexasParser {
 			String str;
 			while ((str = in.nextLine()) != null){
 				//System.out.println(str);
+				String delim = "[ ]+"; // one or more spaces
 				if(str.contains("Profile Line No.")){
 					// found profile lines
-					String[] tokens = str.split("[ ]+");
-					System.out.println(tokens[4]);
+					//String[] tokens = str.split("[ ]+");
+					//int lineNum = Integer.parseInt(tokens[4]);
+					int lineNum = Integer.parseInt(str.split(delim)[4]);
+					//continue;
+					str = in.nextLine();
+					str = in.nextLine();
+					String lineDesc = str.split(": ")[1];
+					str = in.nextLine();
+					str = in.nextLine();
+					str = in.nextLine();
+					str = in.nextLine();
 					
+					List<Point> points = new ArrayList<Point>();
+					
+					while (!str.equals("")){
+						float x = Float.parseFloat(str.split(delim)[1]);
+						float y = Float.parseFloat(str.split(delim)[2]);
+						Point point = new Point(x,y);
+						points.add(point);
+						str = in.nextLine();
+					}
+					String outLine = "No. "+lineNum+": "+lineDesc+" ["+points.size()+"]";
+					System.out.println(outLine);
+					}
+				if(str.equals("\f")){
+					// end of profile lines
+					continue;
 				}
 			}
 			 in.close();
 		} catch(IOException e){
 		}
 	}
-		}
+
+	}
